aae12200a5b6ca53e204d5e1502be65f6b6ec9e1
varad0207 <93907658+varad0207@users.noreply.github.com>
2023-08-15 22:24:38 +0530

    commit 12

--- phpimport/code.php
+++ phpimport/code.php
@@ -1,6 +1,7 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'deltastaar');
+require '../controllers/includes/common.php';
 require 'vendor/autoload.php';
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Settings;
@@ -57,35 +58,53 @@ if (isset($_POST['employee'])) {
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
         $data = $spreadsheet->getActiveSheet()->toArray();
 
-        foreach ($data as $row) 
-        {
-            $emp_code = $row['0'];
-            $fname = $row['1'];
-            $mname = $row['2'];
-            $lname = $row['3'];
-            $designation = $row['4'];
-            $dob = $row['5'];
-            $contact = $row['6'];
-            $address = $row['7'];
-            $state = $row['8'];
-            $country = $row['9'];
-            $pincode = $row['10'];
-            $email = $row['11'];
-            $department = $row['12'];
-            $blood_group = $row['13'];
-            $joining_date = $row['14'];
-            $aadhar_number = $row['15'];
-            $salary = $row['16'];
+        foreach ($data as $row) {
+            $emp_id = $row['0'];
+            $emp_code = $row['1'];
+            $fname = $row['2'];
+            $mname = $row['3'];
+            $lname = $row['4'];
+            $designation = $row['5'];
+            $dob = $row['6'];
+            $contact = $row['7'];
+            $address = $row['8'];
+            $state = $row['9'];
+            $country = $row['10'];
+            $pincode = $row['11'];
+            $email = $row['12'];
+            $department = $row['13'];
+            $blood_group = $row['14'];
+            $joining_date = $row['15'];
+            $aadhar_number = $row['16'];
+            $salary = $row['17'];
+            $room_id = $row['18'];
+           
+            // FINDING DEPARTMENT ID FROM DEPARTMENT NAME
+            $dept_id_fetch = mysqli_query($conn,"SELECT `dept_id` FROM `employee_dept` WHERE `dept_name`='$department'") or die(mysqli_error($conn));
+            $r = mysqli_fetch_assoc($dept_id_fetch);
+            $dept_id = $r['dept_id'];
+            echo $dept_id;
+            // FINDING DESIGNATION ID FROM DESIGNATION NAME
+            $desig_id_fetch = mysqli_query($conn, "SELECT `id` FROM `employee_designation` WHERE `designation`='$designation';");
+            $r = mysqli_fetch_assoc($desig_id_fetch);
+            $desig_id = $r['id'];
+            echo $desig_id;
+
             // insertion code
-            mysqli_query($conn, "INSERT INTO `employee`(`emp_code`, `fname`, `mname`, `lname`, `designation`, `dob`, `contact`, `address`, `state`, `country`, `pincode`, `email`, `department`, `blood_group`, `joining_date`, `aadhaar_number`, `salary`) VALUES ('$emp_code','$fname','$mname','$lname','$designation','$dob','$contact','$address','$state','$country','$pincode','$email','$department','$blood_group','$joining_date','$aadhar_number','$salary')");
+            mysqli_query($conn, "INSERT INTO `employee`(`emp_code`, `fname`, `mname`, `lname`, `designation`, `dob`, `contact`, `address`, `state`, `country`, `pincode`, `email`, `department`, `blood_group`, `joining_date`, `aadhaar_number`, `salary`,`room_id`) VALUES ('$emp_code','$fname','$mname','$lname','$desig_id','$dob','$contact','$address','$state','$country','$pincode','$email','$dept_id','$blood_group','$joining_date','$aadhar_number','$salary','$room_id')") or die(mysqli_error($conn));
             $msg = true;
+
+            echo $emp_id . '      ' . $emp_code . '        ' . $dept_id . '      ' . $desig_id;
         }
+        die;
         if (isset($msg)) {
             $_SESSION['message'] = "Succesfully Imported";
+            unset( $_SESSION['message']);
             header('Location:excel_import.php');
             exit(0);
         } else {
             $_SESSION['message'] = " ! Succesfully Imported";
+            unset( $_SESSION['message']);
             header('Location:excel_import.php');
             exit(0);
         }
@@ -105,34 +124,67 @@ if (isset($_POST['accomodation'])) {
         $inputFileName = $_FILES['accomodation_import']['tmp_name'];
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
         $data = $spreadsheet->getActiveSheet()->toArray();
-
-        foreach ($data as $row) 
-        {
-            $acc_code=$row['0'];
-            $acc_name=$row['1'];
-            $bldg_status=$row['2'];
-            $location=$row['3'];
-            $gender=$row['4'];
-            $tot_capacity=$row['5'];
-            $no_of_rooms=$row['6'];
-            $owner=$row['7'];
-            
-            mysqli_query($conn,"INSERT INTO `accomodation`(`acc_code`, `acc_name`, `bldg_status`, `location`, `gender`, `tot_capacity`, `no_of_rooms`,`owner`) VALUES ('$acc_code','$acc_name','$bldg_status','$location','$gender','$tot_capacity','$no_of_rooms','$owner')");
-            // insertion code
-            $msg = true;
+        // Before the loop
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
         }
+        // Prepare the INSERT statement
+        // $insert_stmt = mysqli_prepare($conn, "INSERT INTO `accomodation`(`acc_code`, `acc_name`, `bldg_status`, `location`, `gender`, `tot_capacity`, `no_of_rooms`, `owner`, `remark`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        // if (!$insert_stmt) {
+        //     die("Error in preparing the INSERT statement: " . mysqli_error($conn));
+        // }
+        foreach ($data as $row) {
+            $acc_code = $row['0'];
+            $acc_name = $row['1'];
+            $bldg_status = $row['2'];
+            $location = $row['3'];
+            // $gender = $row['4'];
+            $gender = ucfirst(strtolower($row['4']));
+            $tot_capacity = $row['5'];
+            $no_of_rooms = $row['6'];
+            $owner = $row['7'];
+            $remark = $row['8'];
+
+            $res = mysqli_query($conn, "SELECT `loc_id` FROM `acc_locations` WHERE `location`='$location'");
+            $r = mysqli_fetch_assoc($res);
+            $location_id = (int)$r['loc_id'];
+
+
+            $submit = mysqli_query($conn, "insert into accomodation (`acc_code`, `acc_name`, `bldg_status`, `location`, `gender`, `tot_capacity`, `no_of_rooms`, `owner`, `remark`) VALUES ('$acc_code', '$acc_name', '$bldg_status', '$location_id', '$gender', '$tot_capacity', '$no_of_rooms', '$owner', '$remark')");
+
+            // $insert_stmt = mysqli_prepare($conn, "INSERT INTO `accomodation`(`acc_code`, `acc_name`, `bldg_status`, `location`, `gender`, `tot_capacity`, `no_of_rooms`, `owner`, `remark`) VALUES ('$acc_code', '$acc_name', '$bldg_status', '$location', '$gender', '$tot_capacity', '$no_of_rooms', '$owner', '$remark'");
+
+
+            // Bind the parameters to the prepared statement
+            // mysqli_stmt_bind_param($insert_stmt, "sssisiiis", $acc_code, $acc_name, $bldg_status, $location_id, $gender, $tot_capacity, $no_of_rooms, $owner, $remark);
+
+            // Execute the prepared statement
+            // $insert_result = mysqli_stmt_execute($insert_stmt);
+
+            if (!$submit) {
+                die("Error in insertion: " . mysqli_error($conn));
+            } else
+                $msg = true;
+        }
+        // Close the prepared statement
+        // mysqli_stmt_close($insert_stmt);
+
         if (isset($msg)) {
-            // $_SESSION['message'] = "Succesfully Imported";
-            // header('Location:excel_import.php');
-            // exit(0);
+            $_SESSION['message'] = "Succesfully Imported";
+            unset( $_SESSION['message']);
+            header('Location:excel_import.php');
+            exit(0);
         } else {
-            // $_SESSION['message'] = " ! Succesfully Imported";
-            // header('Location:excel_import.php');
-            // exit(0);
+            $_SESSION['message'] = " ! Succesfully Imported";
+            unset( $_SESSION['message']);
+            header('Location:excel_import.php');
+            exit(0);
         }
     } else {
-        // $_SESSION['message'] = "Invalid File";
-        // header('Location:excel_import.php');
+        $_SESSION['message'] = "Invalid File";
+        unset( $_SESSION['message']);
+        header('Location:excel_import.php');
     }
 }
 
@@ -142,42 +194,53 @@ if (isset($_POST['vaccination'])) {
     $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
 
     $allowed_ext = ['xls', 'csv', 'xlsx'];
-    if (in_array($file_ext, $allowed_ext)) {
+    if (in_array($file_ext, $allowed_ext)) 
+    {
         $inputFileName = $_FILES['vaccination_import']['tmp_name'];
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
         $data = $spreadsheet->getActiveSheet()->toArray();
 
-        foreach ($data as $row) 
-        {
-            $emp_id=$row[0];
-            $emp_code=$row[1];
-            $category_id=$row[2];
-            $date_of_administration=$row[3];
-            $location=$row[4];
+        foreach ($data as $row) {
+            $v_id = $row['0'];
+            $emp_id = $row['1'];
+            $emp_code = $row['2'];
+            $category_id = $row['3'];
+            $date_of_administration = $row['4'];
+            $location = $row['5'];
 
-            echo $emp_id;
-            echo $emp_code;
-            echo $category_id;
-            echo $date_of_administration;
-            echo $location;
+            echo $v_id.' ';
+            echo $emp_id.' ';
+            echo $emp_code.' ';
+            echo $category_id.' ';
+            echo $date_of_administration.' ';
+            echo $location.' ';
 
-            mysqli_query($conn,"INSERT INTO `vaccination`(`emp_id`, `emp_code`, `category_id`, `date_of_administration`, `location`) VALUES ('$emp_id','$emp_code','$category_id','$date_of_administration','$location')");
+            ?>
+            <br>
+            <?php
+            mysqli_query($conn, "INSERT INTO `vaccination`(`vaccination_id`,`emp_id`, `emp_code`, `category_id`, `date_of_administration`, `location`) VALUES ('$v_id','$emp_id','$emp_code','$category_id','$date_of_administration','$location')") or die(mysqli_error($conn));
+            $msg=true;
         }
-        if (isset($msg)) {
+        
+        if (isset($msg)) 
+        {
             $_SESSION['message'] = "Succesfully Imported";
-
             echo $_SESSION['message'];
+            unset( $_SESSION['message']);
             header('Location:excel_import.php');
             exit(0);
         } else {
             $_SESSION['message'] = " ! Succesfully Imported";
+            unset( $_SESSION['message']);
             header('Location:excel_import.php');
             exit(0);
         }
-    } else {
+    } 
+    else {
         $_SESSION['message'] = "Invalid File";
+        unset( $_SESSION['message']);
         header('Location:excel_import.php');
-     }
+    }
 }
 
 
@@ -192,28 +255,31 @@ if (isset($_POST['rooms'])) {
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
         $data = $spreadsheet->getActiveSheet()->toArray();
 
-        foreach ($data as $row) 
-        {
-            $acc_id=$row['0'];
-            $room_no=$row['1'];
-            $room_capacity=$row['2'];
-            
-            
+        foreach ($data as $row) {
+            $acc_id = $row['0'];
+            $room_no = $row['1'];
+            $room_capacity = $row['2'];
+
+
             // Insert Query
-            mysqli_query($conn,"INSERT INTO `rooms`(`acc_id`,`room_no`,`room_capacity`) VALUES ('$acc_id','$room_no','$room_capacity')");
+            mysqli_query($conn, "INSERT INTO `rooms`(`acc_id`,`room_no`,`room_capacity`) VALUES ('$acc_id','$room_no','$room_capacity')");
             $msg = true;
         }
         if (isset($msg)) {
-            // $_SESSION['message'] = "Succesfully Imported";
-            // header('Location:excel_import.php');
-            // exit(0);
+            $_SESSION['message'] = "Succesfully Imported";
+            unset( $_SESSION['message']);
+            header('Location:excel_import.php');
+            // unset( $_SESSION['message']);
+            exit(0);
         } else {
-            // $_SESSION['message'] = " ! Succesfully Imported";
-            // header('Location:excel_import.php');
-            // exit(0);
+            $_SESSION['message'] = " ! Succesfully Imported";
+            header('Location:excel_import.php');
+            // unset( $_SESSION['message']);
+            exit(0);
         }
     } else {
-        // $_SESSION['message'] = "Invalid File";
-        // header('Location:excel_import.php');
+        $_SESSION['message'] = "Invalid File";
+        unset( $_SESSION['message']);
+        header('Location:excel_import.php');
     }
 }
